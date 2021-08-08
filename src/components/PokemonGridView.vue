3328c25cd8e74ac98b4497604d4587063279999a
Adrian Lucero <adrianrafaellucero9@gmail.com>
2021-08-08 16:39:05 +0800

    PokemonGridView

--- src/components/PokemonGridView.vue
+++ src/components/PokemonGridView.vue
@@ -1,57 +1,16 @@
 <template>
-    <b-container>
-        <Filters />
-        <!-- <div class="pokemon-list-wrapper">
-            <div class="card-wrapper" v-if="list !== null">
-                <div v-for="card in list.data" :key="card.id" >
-                    <h3>{{ card.name }}</h3>
-                    <section  class="demo">
-                        <div class="card" :style="{ 'background-image': 'url('+card.images.small+')' }"></div>
-                    </section>
-                </div>
-            </div>
-            <section class="cards">
-                <div class="card charizard"></div>
-                <div class="card pika"></div>
-                <div class="card mew"></div>
-            </section>
-        </div> -->
-        <!-- <b-row class="demo" v-if="list !== null">
-            <b-col cols="12" sm="3" class="my-1" v-for="card in list.data" :key="card.id">
-                <div class="card-poke" :style="{ 'background-image': 'url('+card.images.small+')' }"></div>
-            </b-col>
-        </b-row> -->
-        <PokemonGridView :list="list" />
-        <Pagination :list="list" />
-    </b-container>
+    <b-row class="demo" v-if="list !== null">
+        <b-col cols="12" sm="3" class="my-1" v-for="card in list.data" :key="card.id">
+            <div class="card-poke" :style="{ 'background-image': 'url('+card.images.small+')' }"></div>
+        </b-col>
+    </b-row>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import Filters from '../components/Filters.vue'
-import Pagination from '../components/Pagination.vue'
-import PokemonGridView from '../components/PokemonGridView.vue'
-
 export default {
-  name: 'PokemonList',
-  components: {
-    Filters,
-    Pagination,
-    PokemonGridView
-  },
-  methods: {
-  },
-  computed: {
-    ...mapGetters({ list: 'pokemontcg/list' })
-  },
-  created () {
-    this.$store.dispatch('pokemontcg/resetFilterParams', {
-      search: '',
-      types: '',
-      rarities: '',
-      sets: ''
-    })
-    this.$store.dispatch('pokemontcg/getList')
+  name: 'PokemonGridView',
+  props: {
+    list: Array
   }
 }
 </script>
